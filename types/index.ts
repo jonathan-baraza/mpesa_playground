6a188d43eddbc5e28b99822f0c09347fa9ab0c39
jonathan_baraza <jbaraza46@gmail.com>
2023-12-30 13:19:27 +0300

    stk push type

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export interface stkBodyType {
+  BusinessShortCode: number;
+  Password: string;
+  Timestamp: string;
+  TransactionType: string;
+  Amount: number;
+  PartyA: number;
+  PartyB: number;
+  PhoneNumber: number;
+  CallBackURL: string;
+  AccountReference: string;
+  TransactionDesc: string;
+}
