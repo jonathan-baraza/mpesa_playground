671da8b831795832a6df4c07d2c9e9b4889d66a7
jonathan_baraza <jbaraza46@gmail.com>
2023-12-30 13:19:53 +0300

    getting access token

--- controllers/mpesa/index.ts
+++ controllers/mpesa/index.ts
@@ -1,14 +1,51 @@
 import { Request, Response, NextFunction } from "express";
 import axios from "axios";
+import { encodedToBase64 } from "../../utils";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const getAccessToken = (
+const CONSUMER_KEY = process.env.CONSUMER_KEY;
+const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
+const AUTHORIZATION_ENDPOINT = process.env.AUTHORIZATION_ENDPOINT || " ";
+
+export const getAccessToken = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    throw new Error("This is a made up error");
-    res.status(200).json({ message: "Getting the access token" });
+    const stringToEncode = `${CONSUMER_KEY}:${CONSUMER_SECRET}`;
+    const encodedString = await encodedToBase64(stringToEncode);
+
+    const response = await axios({
+      method: "GET",
+      url: AUTHORIZATION_ENDPOINT,
+      headers: {
+        Authorization: `Basic ${encodedString}`,
+      },
+    });
+
+    res.status(200).json({ message: "Success true", ...response.data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const stkPush = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: AUTHORIZATION_ENDPOINT,
+      headers: {
+        //  Authorization: `Basic ${encodedString}`,
+      },
+    });
+
+    res.status(200).json({ message: "Sending cash..." });
   } catch (error) {
     next(error);
   }
