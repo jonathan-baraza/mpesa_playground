0b2ad13163ac72bf10b9671a5a93165d26e4f203
jonathan_baraza <jbaraza46@gmail.com>
2023-12-30 15:42:40 +0300

    callback routes

--- routes/mpesa/index.ts
+++ routes/mpesa/index.ts
@@ -1,9 +1,13 @@
 import express, { Router } from "express";
-import { getAccessToken, stkPush } from "../../controllers/mpesa";
+import {
+  getAccessToken,
+  mpesaCallback,
+  stkPush,
+} from "../../controllers/mpesa";
 const router: Router = express.Router();
 
 router.get("/token", getAccessToken);
 router.post("/stk", stkPush);
-
+router.post("/stk-callback", mpesaCallback);
 
 export default router;
